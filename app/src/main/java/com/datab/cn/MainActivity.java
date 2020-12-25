92b8b936f14bced7f3b7691fe07ea850c3b8c18d
caobin10 <1562235042@foxmail.com>
2020-12-25 15:30:42 +0800

    2020.12.25 我们

--- app/src/main/java/com/datab/cn/MainActivity.java
+++ app/src/main/java/com/datab/cn/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //我们
     }
 }
